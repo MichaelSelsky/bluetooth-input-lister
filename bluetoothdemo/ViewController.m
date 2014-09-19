9bba58ce4ed6a5addbb3f4b6b78243cca7d360e2
Michael Selsky <michael.selsky@gmail.com>
2014-09-19 18:14:01 -0400

    Initial commit
    TODO:
    - Create a class that manages all the bluetooth hackery

--- bluetoothdemo/ViewController.m
+++ bluetoothdemo/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
+@import AVFoundation;
+@import MediaPlayer;
+
 @interface ViewController ()
+
 
 @end
 
@@ -16,12 +20,77 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self prepareAudioSession];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    self.view.backgroundColor = [UIColor whiteColor];
+    CGRect frameForMPVV = CGRectMake(50.0, 50.0, 100.0, 100.0);
+    
+    NSArray *availInputs = [[AVAudioSession sharedInstance] availableInputs];
+    int count = [availInputs count];
+    NSMutableArray *arr = [NSMutableArray new];
+    for (int k = 0; k < count; k++) {
+        AVAudioSessionPortDescription *portDesc = [availInputs objectAtIndex:k];
+        NSLog(@"input%i port type %@", k+1, portDesc.portType);
+        NSLog(@"input%i port name %@", k+1, portDesc.portName);
+        [arr addObject:portDesc.portName];
+    }
+    
+    UITextView *textView = [[UITextView alloc] initWithFrame:frameForMPVV];
+    textView.text = [NSString stringWithFormat:@"%@", arr];
+    [self.view addSubview:textView];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(myInterruptionSelector:)
+                                                 name:AVAudioSessionInterruptionNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(myRouteChangeSelector:)
+                                                 name:AVAudioSessionRouteChangeNotification
+                                               object:nil];
+}
+
+- (void)myRouteChangeSelector:(NSNotification*)notification {
+    AVAudioSessionRouteDescription *currentRoute = [[AVAudioSession sharedInstance] currentRoute];
+    NSArray *inputsForRoute = currentRoute.inputs;
+    NSArray *outputsForRoute = currentRoute.outputs;
+    AVAudioSessionPortDescription *outPortDesc = [outputsForRoute objectAtIndex:0];
+    NSLog(@"current outport type %@", outPortDesc.portType);
+    AVAudioSessionPortDescription *inPortDesc = [inputsForRoute objectAtIndex:0];
+    NSLog(@"current inPort type %@", inPortDesc.portType);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (BOOL)prepareAudioSession {
+    BOOL success = [[AVAudioSession sharedInstance] setActive:NO error:nil];
+    
+    if (!success) {
+        NSLog(@"deactivationError");
+    }
+    
+    // set audio session category AVAudioSessionCategoryPlayAndRecord options AVAudioSessionCategoryOptionAllowBluetooth
+    success = [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionAllowBluetooth error:nil];
+    if (!success) {
+        NSLog(@"setCategoryError");
+    }
+    
+    // activate audio session
+    success = [[AVAudioSession sharedInstance] setActive:YES error: nil];
+    if (!success) {
+        NSLog(@"activationError");
+    }
+    
+    return success;
+}
+
+
+
+
 
 @end
